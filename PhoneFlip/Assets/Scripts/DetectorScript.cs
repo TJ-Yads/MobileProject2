d88802d1667286cb8e3b8f6578d8a1e7689443eb
Anthony DeCenzo <9rarwolf9@gmail.com>
2021-03-06 11:46:16 -0500

    Added Unity project
    Contains a prototype for the flipping and dragging mechanics.

---
+++ PhoneFlip/Assets/Scripts/DetectorScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectorScript : MonoBehaviour
+{
+    public Collider2D myCollider;
+    public bool noBlockZone;
+
+    void Update()
+    {
+        if (CheckDetection())
+        {
+        }
+    }
+
+    public bool CheckDetection()
+    {
+        if (myCollider.IsTouchingLayers(LayerMask.GetMask("Blocks")))
+        {
+            if (noBlockZone)
+            {
+                return (false);
+            }
+            else
+            {
+                return (true);
+            }
+            
+        }
+        else
+        {
+            if (noBlockZone)
+            {
+                return (true);
+            }
+            else
+            {
+                return (false);
+            }
+        }
+    }
+}
---
+++ PhoneFlip/Assets/Scripts/MoveScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    bool hover = false;
+
+    public Rigidbody2D myBody;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && hover)
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = mousePosition;
+            myBody.gravityScale = 0;
+        }
+        else
+        {
+            myBody.gravityScale = 1;
+        }
+    }
+
+    void OnMouseEnter()
+    {
+        hover = true;
+    }
+
+    void OnMouseExit()
+    {
+        hover = false;
+        Debug.Log("False");
+    }
+
+
+/*    void OnMouseDown()
+    {
+
+    }*/
+/*    Vector2 oldPosition;
+
+
+
+    void OnMouseDown()
+    {
+        oldPosition = Input.mousePosition;
+    }
+
+    void OnMouseDrag()
+    {
+        Vector2 currentPosition = Input.mousePosition;
+        Vector2 movedPosition = currentPosition - oldPosition;
+
+        transform.position = movedPosition;
+        oldPosition = currentPosition;
+    }*/
+}
